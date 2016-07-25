2643343eb088491ac55c60324e0a5d695d649b74
sstephanos <simons@simons-macbook-pro.local>
2016-07-25 10:48:26 -0500

    woot

--- TableViewHackwich/ViewController.swift
+++ TableViewHackwich/ViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
     var cities = ["Chicago", "Denver", "Seattle"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-
+        
+        }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return cities.count
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = cities[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            cities.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
 }

c0630394f0e5af844aaf7671dc984ffe1abe3109
sstephanos <simons@simons-macbook-pro.local>
2016-07-25 11:06:16 -0500

    allow for entries to be added

--- TableViewHackwich/ViewController.swift
+++ TableViewHackwich/ViewController.swift
@@ -34,4 +34,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             tableView.reloadData()
         }
     }
+    
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem){
+        let alert = UIAlertController(title: "Add City", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Add City Here"
+        }
+    
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in let cityTextField = alert.textFields![0] as UITextField
+        self.cities.append(cityTextField.text!)
+        self.tableView.reloadData()
+        
+    }
+    
+    alert.addAction(addAction)
+    self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
